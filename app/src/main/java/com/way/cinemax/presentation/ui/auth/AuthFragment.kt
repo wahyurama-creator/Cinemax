05cebd108356bdbc28573ba88c63f834b3f49ae7
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-04-27 22:10:14 +0700

    Create fragment_auth

---
+++ app/src/main/java/com/way/cinemax/presentation/ui/auth/AuthFragment.kt
@@ -0,0 +1,18 @@
+package com.way.cinemax.presentation.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.way.cinemax.R
+
+class AuthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+}
